4a514f339d9ac9fd05b7073a3eef2cdad8f5ad62
Wala Eddine <135642375+WalaEddine01@users.noreply.github.com>
2026-01-17 09:24:28 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,21 +7,12 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
+  output: 'export',
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
 }
+
 
 mergeConfig(nextConfig, userConfig)
 
